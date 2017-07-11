368f6c0f175fb8f3f3745887f95ede1fa2bb40fa
dingyang <dingyang@baidu.com>
2017-07-11 23:46:03 +0800

    add wxspider codes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var appIndexApi = express();
-
+// var wxControl = require('../control/wx');
 
 appIndexApi.get('/test', function(req, res) {
+    // wxControl.starSpider();
     res.json({
         "name": 'dingyang',
         "age": '27'
